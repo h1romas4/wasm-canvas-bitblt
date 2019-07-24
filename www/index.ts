5e2e6baa9d56e268e8703bc9aea4c45a08a8ea6b
Hiromasa Tanaka <ISOL@ISOL-THINK>
2019-07-24 11:43:01 +0900

    change style

--- www/index.ts
+++ www/index.ts
@@ -10,12 +10,12 @@ class Bitblt {
     static readonly RGBA = 4;
 
     private canvas: HTMLCanvasElement;
-    private canvas_context: CanvasRenderingContext2D;
-    private canvas_image_data: ImageData;
+    private canvasContext: CanvasRenderingContext2D;
+    private canvasImageData: ImageData;
     private screen: Screen;
-    private image_data: Uint8Array;
+    private imageData: Uint8Array;
 
-    private anime_id: number;
+    private animeId: number;
 
     /**
      * constructor
@@ -29,12 +29,12 @@ class Bitblt {
             this.canvas.style.width = 320 + "px";
             this.canvas.style.height = 240 + "px";
         }
-        this.canvas_context = this.canvas.getContext('2d');
-        this.canvas_image_data = this.canvas_context.createImageData(
+        this.canvasContext = this.canvas.getContext('2d');
+        this.canvasImageData = this.canvasContext.createImageData(
             Bitblt.CANVAS_WIDTH, Bitblt.CANVAS_HEIGHT);
 
         this.screen = new Screen(Bitblt.CANVAS_WIDTH, Bitblt.CANVAS_HEIGHT);
-        this.image_data = new Uint8Array(
+        this.imageData = new Uint8Array(
             memory.buffer,
             this.screen.get_canvas_bitmap_ptr(),
             Bitblt.CANVAS_WIDTH * Bitblt.CANVAS_HEIGHT * Bitblt.RGBA);
@@ -48,10 +48,10 @@ class Bitblt {
         this.screen.update();
         // draw
         this.screen.draw();
-        this.canvas_image_data.data.set(this.image_data);
-        this.canvas_context.putImageData(this.canvas_image_data, 0, 0);
+        this.canvasImageData.data.set(this.imageData);
+        this.canvasContext.putImageData(this.canvasImageData, 0, 0);
         // next tick
-        this.anime_id = requestAnimationFrame(() => this.loop());
+        this.animeId = requestAnimationFrame(() => this.loop());
     }
 }
 
